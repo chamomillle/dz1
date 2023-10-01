aa9d060af65932a2f11f92f9e2b33f7fc86b0e6d
chamomillle <marina10032005@yandex.ru>
2023-10-01 14:09:43 +0300

    last2

---
+++ dz1/2.7.py
@@ -0,0 +1,10 @@
+from random import randint
+comp_number = randint(0, 100)
+your_number = int(input())
+while comp_number != your_number:
+    if comp_number > your_number:
+        print('загаданное число больше!', end='\n')
+    else:
+        print('загаданное число меньше!', end='\n')
+    your_number = int(input())
+print('вы угадали число!')
